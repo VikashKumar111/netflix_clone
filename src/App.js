e9844ad79330c1364070934620f35c0e95dcf520
Vikash Kumar <vikasthakur9310@gmail.com>
2023-08-09 00:18:45 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-
+import React from 'react';
 import './App.css';
+import Row from './Row';
 
 function App() {
   return (
     <div className="App">
       <h1>Hey clever programmers we are going to build netflix clone today</h1>
+      <Row title="NETFLIX_ORIGINALS" />
+       <Row title="Trending Now" />
 
      
     </div>
