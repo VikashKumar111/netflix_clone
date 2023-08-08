f10d647b667f2cd37e9d355da3c25947b9a85e75
Vikash Kumar <vikasthakur9310@gmail.com>
2023-08-08 23:32:55 +0530

    add

--- src/App.js
+++ src/App.js
@@ -6,15 +6,7 @@ function App() {
     <div className="App">
       <h1>Hey clever programmers we are going to build netflix clone today</h1>
 
-      <Row title="NETFLIX ORIGINALS" />
-      <Row title="Trending Now" />
-      <Row title="Top Rated" />
-      <Row title="NETFLIX ORIGINALS" />
-      <Row title="Trending Now" />
-      <Row title="Top Rated" />
-      <Row title="NETFLIX ORIGINALS" />
-      <Row title="Trending Now" />
-       <Row title="Top Rated"/>
+     
     </div>
   );
 }
