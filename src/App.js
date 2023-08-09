8dc5e390a1514c451538ee2b03b9f615bca1b5b7
Vikash Kumar <vikasthakur9310@gmail.com>
2023-08-09 15:52:53 +0530

    fetched the movies

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import Row from './Row';
+import requests from './requests';
 
 function App() {
   return (
     <div className="App">
       <h1>Hey clever programmers we are going to build netflix clone today</h1>
-      <Row title="NETFLIX_ORIGINALS" />
-       <Row title="Trending Now" />
+      <Row title="NETFLIX_ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
 
      
     </div>
--- src/Row.js
+++ src/Row.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { useEffect,useState } from 'react';
+import axios from './axios';
 
-const Row = ({title}) => {
+
+const Row = ({ title, fetchUrl }) => {
+    const [movies, setMovies] = useState([]);
+   
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(fetchUrl);
+            setMovies(request.data.results);
+            return request;
+        }
+        fetchData();
+    }, [fetchUrl]);
+
+    console.log(movies);
+
   return (
-      <div>
+      <div className='row'>
           <h2>{title}</h2>
+          <div className='row__posters'>
+              {movies.map(movie => (
+                  <img src={movie.poster_path} alt={movie.name}/>
+             ))} 
+          </div>
       </div>
   )
 }
