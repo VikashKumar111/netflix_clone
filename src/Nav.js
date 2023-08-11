2af91da1911210f2adbb98d49df6f6d4f23972b3
Vikash Kumar <vikasthakur9310@gmail.com>
2023-08-11 17:09:20 +0530

    build the navbar

--- src/Nav.js
+++ src/Nav.js
@@ -1,9 +1,23 @@
-import React from 'react';
+
+import React,{useState,useEffect} from 'react';
 import './Nav.css';
 
 const Nav = () => {
+    const [show, handleShow] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 100) {
+                handleShow(true);
+            } else handleShow(false);
+        });
+        return () => {
+            window.removeEventListener("scroll",null);
+        };
+    }, []);
+
   return (
-      <div className='nav'>
+      <div className={`nav ${show && "nav__black"}`}>
           <img
               className='nav__logo'
               src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/799px-Netflix_2015_logo.svg.png?20190206123158'
